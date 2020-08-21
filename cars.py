41add518091591b1139257db1977837b8b756de2
Gerlane <gerlane_andrade@yahoo.com.br>
2020-08-21 13:36:36 -0300

    Política

--- cars.py
+++ cars.py
@@ -47,6 +47,7 @@ class Vehicle:
             if self.type == 'green' and self.emissions() < self.firm.sim.e:
                 policy_value = params.green_support[self.firm.sim.policy['level']] * e_parameter
         # TODO: Although sales price is full, probably firms should receive value deduced from iva and policy tax?
+        # As firmas só recebem a Política Tx e pagam o IVA que vai pro gove
         self.sales_price = (1 + params.iva) * (1 + params.p_lambda) * \
                            (1 + policy_tax) * self.production_cost + policy_value
         return policy_tax * self.production_cost + policy_value
